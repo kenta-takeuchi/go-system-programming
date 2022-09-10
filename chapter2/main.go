1b117c977879ee2a27650ac42571fff5c69ac189
kenta-takeuchi <49678973+kenta-takeuchi@users.noreply.github.com>
2022-09-10 14:34:14 +0900

    os write

--- chapter2/main.go
+++ chapter2/main.go
@@ -1,23 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"net"
+	"os"
 )
 
-type Talker interface {
-	Talk()
-}
-
-type Greeter struct {
-	name string
-}
-
-func (g Greeter) Talk() {
-	fmt.Printf("Hello, my name is %s\n", g.name)
-}
-
 func main() {
-	var talker Talker
-	talker = &Greeter{"wozozo"}
-	talker.Talk()
+	conn, err := net.Dial("tcp", "example.com:80")
+	if err != nil {
+		panic(err)
+	}
+
+	io.WriteString(conn, "GET / HTTP/1.0\r\nHost: example.com\r\n\r\n")
+	io.Copy(os.Stdout, conn)
 }
